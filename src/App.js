2da904d0352c21cb350a615436d010587de02a8b
Peter <warrenp11@gmail.com>
2022-03-11 11:03:49 -0500

    Hardcode initial Gallery component Name and Description

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import About from "./components/About";
 import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div>
-      <Nav />
+      <Nav></Nav>
       <main>
         <About></About>
+        <Gallery></Gallery>
       </main>
     </div>
   );
---
+++ src/components/Gallery/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {capitalizeFirstLetter} from "../../utils/helpers";
+import photo from "../../assets/small/projects/0.jpg";
+
+function Gallery() {
+    return (
+        <section>
+            <h1>NAME</h1>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+export default Gallery;
