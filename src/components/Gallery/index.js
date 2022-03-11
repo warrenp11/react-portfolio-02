cd0df8f7219030642a57e73fc3ed2054977d763a
Peter <warrenp11@gmail.com>
2022-03-11 11:12:16 -0500

    Gallery component displaying first project

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,14 +1,27 @@
 import React from "react";
-import {capitalizeFirstLetter} from "../../utils/helpers";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 import photo from "../../assets/small/projects/0.jpg";
 
 function Gallery() {
-    return (
-        <section>
-            <h1>NAME</h1>
-            <p>DESCRIPTION</p>
-        </section>
-    );
+  const currentCategory = {
+    name: "portfoltio",
+    description:
+      "these are a few of the projects I've created as a web developer",
+  };
+
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{capitalizeFirstLetter(currentCategory.description)}</p>
+      <div className="flex-row">
+        <img
+          src={photo}
+          alt="Coding Quiz Thumbnail"
+          className="img-thumbnail mx-1"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Gallery;
